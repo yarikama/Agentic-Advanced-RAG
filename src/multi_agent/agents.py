a5397fa7d6964a7ea3c7a393d32fa839cbb650fa
yarikama <amaki30226@gmail.com>
2025-01-12 20:48:37 +0800

    add dedent

--- src/multi_agent/agents.py
+++ src/multi_agent/agents.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from .tools import Tools
 import config.constants as const 
+from textwrap import dedent
 
 class Agents:
     def __init__(
@@ -78,17 +79,17 @@ class Agents:
     def _classifier(self):
         return Agent(
             role='Query Classifier',
-            goal="""Accurately determine if a query requires information retrieval or can be answered directly.
+            goal=dedent("""
+            Accurately determine if a query requires information retrieval or can be answered directly.
             Your primary function is to 
             examine incoming queries and decide whether they need external information retrieval 
             or can be answered with existing knowledge. You understand the nuances of different 
             query types and can distinguish between those requiring fact lookup and those that 
-            can be handled with general knowledge or language processing.""",
-            backstory="""
+            can be handled with general knowledge or language processing."""),
+            backstory=dedent("""
             You are an AI specialist with years of experience in natural language processing. 
             Your expertise lies in understanding the nuances of human queries and efficiently 
-            categorizing them for optimal processing.
-            """,
+            categorizing them for optimal processing."""),
             verbose=const.CREWAI_AGENT_VERBOSE,
             llm=self.llm,
             memory=const.CREWAI_AGENT_MEMORY,
@@ -98,15 +99,15 @@ class Agents:
     def _plan_coordinator(self):
         return Agent(
             role='Plan Coordinator',
-            goal="""
+            goal=dedent("""
             For user queries, you need to define objectives, create step-by-step plans,
             and coordinate the execution of tasks among different agents.
-            """,
-            backstory="""
+            """),
+            backstory=dedent("""
             You are a seasoned project manager with a knack for breaking down complex tasks 
             into manageable steps. Your experience in coordinating diverse teams makes you 
             the perfect fit for organizing the efforts of various specialized agents.
-            """,
+            """),
             verbose=const.CREWAI_AGENT_VERBOSE,
             llm=self.llm,
             memory=const.CREWAI_AGENT_MEMORY,
@@ -116,15 +117,16 @@ class Agents:
     def _query_processor(self):
         return Agent(
             role='Query Processor',
-            goal="""Optimize user queries for enhanced information retrieval and analysis.
+            goal=dedent("""
+            Optimize user queries for enhanced information retrieval and analysis.
             You decompose complex ones, transform and optimize them for better retrieval, 
             and break them into manageable sub-queries when needed.
-            """,
-            backstory="""
+            """),
+            backstory=dedent("""
             You are a linguistic expert with a deep understanding of query optimization techniques. 
             Your background in computational linguistics allows you to effortlessly transform and 
             decompose complex queries into their most effective forms.
-            """,
+            """),
             verbose=const.CREWAI_AGENT_VERBOSE,
             llm=self.llm,
             memory=const.CREWAI_AGENT_MEMORY,
@@ -134,17 +136,17 @@ class Agents:
     def _topic_searcher(self):
         return Agent(
             role='Topic Searcher',
-            goal="""
+            goal=dedent("""
             Receive community information related to the user_query, sorted in descending order of relevance.
             Analyze and synthesize this information to formulate potential answers that could help address the user_query.
             Generate a summary of key points derived from the provided community data.
             If the provided community information is insufficient to generate meaningful key points or potential answers, clearly state this limitation.
-            """,
-            backstory="""
+            """),
+            backstory=dedent("""
             You are a seasoned librarian with a talent for organizing and categorizing information. 
             Your years of experience in managing vast amounts of data have honed your skills in 
             quickly identifying key topics and their relationships.
-            """,
+            """),
             verbose=const.CREWAI_AGENT_VERBOSE,
             llm=self.llm,
             memory=const.CREWAI_AGENT_MEMORY,
@@ -154,15 +156,16 @@ class Agents:
     def _retriever(self):
         return Agent(
             role='Retriever',
-            goal="""Retrieve relevant information from the database for given sub-queries.
+            goal=dedent("""
+            Retrieve relevant information from the database for given sub-queries.
             You search the database for topics and details related to the user query, 
             gather relevant information, and compile results for the Generator.
-            """,
-            backstory="""
+            """),
+            backstory=dedent("""
             You are a skilled data analyst with a background in information retrieval systems. 
             Your ability to quickly sift through large datasets and pinpoint relevant information 
             is unparalleled.
-            """,
+            """),
             verbose=const.CREWAI_AGENT_VERBOSE,
             llm=self.llm,
             memory=const.CREWAI_AGENT_MEMORY,
@@ -172,13 +175,13 @@ class Agents:
     def _synthesizer(self):
         return Agent(
             role='Synthesizer',
-            goal="""
+            goal=dedent("""
             Summarize and synthesize information from various communities accurately, without fabrication or alteration of original content.
-            Generate creative, speculative answers based on the synthesized information to address user queries.""",
-            backstory="""
+            Generate creative, speculative answers based on the synthesized information to address user queries."""),
+            backstory=dedent("""
             An experienced information synthesis analyst skilled in extracting key insights from diverse data sources, 
             identifying patterns, and generating both factual summaries and creative interpretations.
-            """,
+            """),
             verbose=const.CREWAI_AGENT_VERBOSE,
             llm=self.llm,
             memory=const.CREWAI_AGENT_MEMORY,
@@ -188,13 +191,16 @@ class Agents:
     def _information_organizer(self):
         return Agent(
             role='Information Organizer',
-            goal="""Your task is to structure and consolidate the retrieved data and community information, preserving the original tone and avoiding any fabrication. Your organized output will serve as a foundation for subsequent response generation by other agents.""",
-            backstory="""
+            goal=dedent("""
+            Your task is to structure and consolidate the retrieved data and community information, preserving the original tone and avoiding any fabrication. 
+            Your organized output will serve as a foundation for subsequent response generation by other agents.
+            """),
+            backstory=dedent("""
             You are an AI expert in information organization, with a talent for structuring and consolidating data.
             You excel at distilling complex data into clear, concise summaries based on user queries.
             Your unique ability to identify key concepts and connect diverse information has aided numerous breakthroughs across various fields.
             You approach each task eagerly, seeing it as a chance to uncover and present crucial insights that empower decision-makers and innovators.
-            """,
+            """),
             verbose=const.CREWAI_AGENT_VERBOSE,
             llm=self.llm,
             memory=const.CREWAI_AGENT_MEMORY,
@@ -206,12 +212,12 @@ class Agents:
         return Agent(
             role='Generator',
             goal='Analyze data and generate insights on the data retrieved to response to user query.',
-            backstory="""
+            backstory=dedent("""
             You are a veteran analyst with decades of experience in data interpretation and query resolution. 
             Your career in both academia and industry has honed your ability to swiftly connect user queries 
             with relevant information, distilling complex data into clear, accurate answers. Known for your 
             precision and insight, you excel at crafting responses that directly address the heart of each query.
-            """,
+            """),
             verbose=const.CREWAI_AGENT_VERBOSE,
             llm=self.llm,
             memory=const.CREWAI_AGENT_MEMORY,
@@ -221,16 +227,17 @@ class Agents:
     def _response_auditor(self):
         return Agent(
             role='Response Auditor',
-            goal="""Ensure alignment between user query, conclusions, and task outcome.
+            goal=dedent("""
+            Ensure alignment between user query, conclusions, and task outcome.
             You review the query and conclusions, assess relevance and completeness, 
             identify gaps, and approve or recommend improvements.
-            """,
-            backstory="""
+            """),
+            backstory=dedent("""
             You are a seasoned quality assurance expert with a keen eye for detail. Your background 
             in both data analysis and customer service has made you adept at evaluating responses 
             for accuracy, relevance, and completeness. You take pride in your ability to spot 
             discrepancies and suggest improvements, ensuring that every answer meets the highest standards.
-            """,
+            """),
             verbose=const.CREWAI_AGENT_VERBOSE,
             llm=self.llm,
             memory=const.CREWAI_AGENT_MEMORY,
@@ -241,12 +248,12 @@ class Agents:
         return Agent(
             role='Database Updater',
             goal='store the task(query) defined by user and the conclusions from your co=workers in the database.',
-            backstory="""
+            backstory=dedent("""
             You are a meticulous data curator with extensive experience in information management. 
             Your expertise lies in efficiently organizing and storing complex data. You have a 
             talent for structuring information in ways that enhance its accessibility and usefulness 
             for future queries, ensuring that the knowledge base remains up-to-date and valuable.
-            """,
+            """),
             verbose=const.CREWAI_AGENT_VERBOSE,
             llm=self.llm,
             memory=const.CREWAI_AGENT_MEMORY,
